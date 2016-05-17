681a2457a5145d8991d41f766304dfcc26291930
Eric Lin <ericsuhelin@gmail.com>
2016-05-16 22:10:27 -0400

    implemented transitions

--- index_scripts.js
+++ index_scripts.js
@@ -1,53 +1,73 @@
 
-	//PARALLAX IS INTERFERING WITH THE SMOOTH SCROLLING
+//PARALLAX IS INTERFERING WITH THE SMOOTH SCROLLING
 
-	//parallax for jumbotron
-	/*var jumboHeight = $('.jumbotron').outerHeight();
-	$(window).scroll(function(e){
-		parallax();
-	});
+//parallax for jumbotron
+/*var jumboHeight = $('.jumbotron').outerHeight();
+$(window).scroll(function(e){
+	parallax();
+});
 
-	function parallax(){
-		var scrolled=$(window).scrollTop();
-		$('.jumbotron').css('height', (jumboHeight-scrolled) + 'px');
-	}*/
+function parallax(){
+	var scrolled=$(window).scrollTop();
+	$('.jumbotron').css('height', (jumboHeight-scrolled) + 'px');
+}*/
 
-	//smooth scrolling on page
-	$(function(){
-		$('#menu a').click(function(e){
-			e.preventDefault();
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				console.log(this.hash);
-				console.log(target);
-				//target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-				if(target.length){
-					$('html, body').animate({
-						scrollTop: target.offset().top}, 950);
-					return false;
-				}
+//smooth scrolling on page
+/*
+$(function(){
+	$('#menu a').click(function(e){
+		e.preventDefault();
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			console.log(this.hash);
+			console.log(target);
+			//target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if(target.length){
+				$('html, body').animate({
+					scrollTop: target.offset().top}, 950);
+				return false;
 			}
-		})
-	});
+		}
+	})
+});
+*/
 
 
-
-	/*
-		Clipboard Copy function
-	*/
-	$(document).ready(function(){
-		var clipboard = $('#clipboardcopy');
-		clipboard.on('click', function(e){
-			clipboard.text('Copied!');
-			e.preventDefault();
-			clipboard.clipboard({
-				path: 'resources/jquery.clipboard.swf',
-				copy: function(){
-					clipboard.text('...');
-            		return 'esl89@cornell.edu';
-				}
-			});
+/*
+	Clipboard Copy function
+*/
+$(document).ready(function(){
+	var clipboard = $('#clipboardcopy');
+	clipboard.on('click', function(e){
+		clipboard.text('Copied!');
+		e.preventDefault();
+		clipboard.clipboard({
+			path: 'resources/jquery.clipboard.swf',
+			copy: function(){
+				clipboard.text('...');
+        		return 'esl89@cornell.edu';
+			}
 		});
-
-		
 	});
+
+	
+});
+
+var sections = ["#top", "#Introduction", "#Education", "#Projects", "#About", "#Contact"]
+function hideAll() {
+	for (let section of sections) {
+		$(section).hide()
+	}
+}
+function hideAllBut(section) {
+	hideAll();
+	$(section).fadeIn();
+}
+
+var menu_items = ["#menu-top", "#menu-intro", "#menu-education", "#menu-projects", "#menu-about", "#menu-contact"]
+for (let menu_item of menu_items) {
+	$(menu_item).click(function(e){
+		e.preventDefault();
+		hideAllBut($(menu_item).attr('href'));
+	})
+}
